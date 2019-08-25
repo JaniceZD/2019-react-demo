667c41e43d1a808dc4f48ef0390312acca9bfa23
Janice <mnzd123@gmail.com>
2019-08-25 18:05:53 +0800

    第一次提交应用

---
+++ build/precache-manifest.a5990908ea41fb7bd22b8838cc45509e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c0c8c6bace7465e356843e05872957c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c290a3fe432fb9550cd",
+    "url": "/static/css/main.a6a3ea82.chunk.css"
+  },
+  {
+    "revision": "7a6b3bd31b82aaaa9413",
+    "url": "/static/js/2.23896bd1.chunk.js"
+  },
+  {
+    "revision": "9c290a3fe432fb9550cd",
+    "url": "/static/js/main.3241744d.chunk.js"
+  },
+  {
+    "revision": "463d011fc12c11eae559",
+    "url": "/static/js/runtime~main.43797eec.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -2,22 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function sayHi() {
+  alert('Hi!')
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">React 也不过如此嘛</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          编辑 <code>src/App.js</code> 就能改变页面显示。
+        <button onClick={sayHi}>点我!</button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
